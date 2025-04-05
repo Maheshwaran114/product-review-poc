dc3efb8fa0b9b10d0e2b040eee62f521f4b168a8
umamaheswaran <33101565+Maheshwaran114@users.noreply.github.com>
2025-04-06 02:37:11 +0530

    ci/cd v8

--- frontend/frontend-app/src/App.test.js
+++ frontend/frontend-app/src/App.test.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Product Review POC header', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const headerElement = screen.getByText(/Product Review POC/i);
+  expect(headerElement).toBeInTheDocument();
 });
